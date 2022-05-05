611bd34be0dbff0eedbe2e818e35ca3e0c4f76df
duck-bongos <billmannd@gmail.com>
2022-05-05 11:32:46 -0400

    added data, EDA file

---
+++ eda.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+import pandas as pd
+
+if __name__ in "__main__":
+    df = pd.read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
+    df["submission_date"] = df["submission_date"].apply(
+        lambda x: datetime.strptime(x, "%m/%d/%Y")
+    )
+    print(df.head())
