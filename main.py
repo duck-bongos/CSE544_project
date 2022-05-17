670dbd295590061509901ec4e39457781b981270
Andrew Burford <andrewsburford@gmail.com>
2022-05-16 20:05:34 -0400

    perform cases/deaths data cleaning in main.py

--- main.py
+++ main.py
@@ -2,11 +2,33 @@ from datetime import datetime
 import pandas as pd
 from utilities import tukey
 from vax import *
+import sys
 
 # our states are Massachussetts and Mississippi.
 GROUP_11_STATES = ["MA", "MS"]
 
 if __name__ in "__main__":
+	df = pd.read_csv("United_States_COVID-19_Cases_and_Deaths_by_State_over_Time.csv")
+	df = df.rename(columns={"submission_date": "date", 'tot_cases': 'cases', 'tot_death': 'deaths'})
+	df['date'] = pd.to_datetime(df['date'])
+	df.sort_values('date', inplace=True)
+	dfs = {st: {} for st in GROUP_11_STATES}
+	for state in GROUP_11_STATES:
+		for col in ['cases', 'deaths']:
+			print('Cleaning %s data in state %s' % (col, state))
+			st = df[['date', 'state', col]]
+			st = st[st['state'] == state]
+			st[col] = st[col].diff()
+			stlen = st.shape[0]
+			st.dropna(inplace=True)
+			print('Missing %d out of %d days of data' % (stlen - st.shape[0] - 1, stlen))
+			print("Applying Tukey's rule")
+			stlen = st.shape[0]
+			st = tukey(st, col)
+			print("Rows with outliers: %d" % (stlen - st.shape[0]))
+			dfs[state][col] = st
+		print()
+
 	vax = pd.read_csv("COVID-19_Vaccinations_in_the_United_States_Jurisdiction.csv")
 	vax = vax.rename(columns={"Location": "state", "Date": "date", 'Administered': 'admin'})
 	vax["date"] = pd.to_datetime(vax["date"])
